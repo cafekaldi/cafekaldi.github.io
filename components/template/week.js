cd94ceb63fbe97cf376feec92566c05941156001
dialmedu <diegomesa.1414@gmail.com>
2017-07-05 16:22:15 -0500

    Agregada Listado de Canciones

--- components/template/week.js
+++ components/template/week.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
 
 	let duracionAnimacion = 4500;
 	setTimeout( openGaleryDayOfWeek(),duracionAnimacion);
+	openPlayListDayOfWeek();
 });
 
 
@@ -45,6 +46,7 @@ function viewGalery(dayOfWeek)
 	if (dayOfWeek == "especial") {
 		if ($('#especial').hasClass('active')) {
 			$('#'+dayOfWeek).addClass('open');
+			$('#'+dayOfWeek).carousel();
 		}
 		else
 		{
@@ -52,6 +54,7 @@ function viewGalery(dayOfWeek)
 		}
 	} else if (!$('#'+dayOfWeek).hasClass('open')) {
 		$('#'+dayOfWeek).addClass('open');
+		$('#'+dayOfWeek).carousel();
 	}
 
 	
@@ -62,4 +65,121 @@ function openGaleryDayOfWeek()
 {
 	let fecha = new Date();
 	$($('.galeria').find('.carousel')[fecha.getDay() - 1]).addClass('open');
+}
+
+var listaMusica = new Array();
+var playList = new Array();
+
+function openPlayListDayOfWeek()
+{
+	let fecha = new Date();
+	listaMusica[0]  = [
+			{
+				source : 'public/music/lunes/Hotel Carlifornia.mp3',
+				name: 'Hotel Carliforni',
+			},
+			{
+				source : 'public/music/lunes/Killing me softly.mp3',
+				name: 'Killing me softly',
+			},
+			{
+				source : 'public/music/lunes/Maroon 5 - Sunday Morning.mp3',
+				name: 'Maroon 5 - Sunday Morning',
+			},
+			{
+				source : 'public/music/lunes/Phil Collins - True Colors.mp3',
+				name: 'Phil Collins - True Colors',
+			},
+			{
+				source : 'public/music/lunes/Radiohead - Creep.mp3',
+				name: 'Radiohead - Creep',
+			},
+			{
+				source : 'public/music/lunes/U2 - With Or Without You.mp3',
+				name: 'U2 - With Or Without You',
+			}
+	];
+	listaMusica[1]  = [
+			{
+				source : 'public/music/martes/Green Day- Boulevard Of Broken Dreams.mp3',
+				name: 'Hotel Carliforni',
+			},
+			{
+				source : 'public/music/martes/Guns N Roses - November Rain.mp3',
+				name: 'Guns N Roses - November Rain',
+			},
+			{
+				source : 'public/music/martes/Guns N Roses - Sweet Child O Mine.mp3',
+				name: 'Guns N Roses - Sweet Child O Mine',
+			}
+	];
+	listaMusica[2]  = [
+			{
+				source : 'public/music/miercoles/Camila - De Que Me Sirve La Vida.mp3',
+				name: 'Camila - De Que Me Sirve La Vida',
+			},
+			{
+				source : 'public/music/miercoles/Fonseca - Sorprenderte.mp3',
+				name: 'Fonseca - Sorprenderte',
+			},
+			{
+				source : 'public/music/miercoles/Jesse & Joy - Me Voy.mp3',
+				name: 'Jesse & Joy - Me Voy',
+			},
+			{
+				source : 'public/music/miercoles/Juanes - Hermosa Ingrata.mp3',
+				name: 'Juanes - Hermosa Ingrata',
+			},
+
+	];
+	listaMusica[3]  = [
+			{
+				source : 'public/music/lunes/Hotel Carlifornia.mp3',
+				name: 'Hotel Carliforni',
+			},
+			{
+				source : 'public/music/lunes/Killing me softly.mp3',
+				name: 'Killing me softly',
+			}
+	];
+	listaMusica[4]  = [
+			{
+				source : 'public/music/lunes/Hotel Carlifornia.mp3',
+				name: 'Hotel Carliforni',
+			},
+			{
+				source : 'public/music/lunes/Killing me softly.mp3',
+				name: 'Killing me softly',
+			}
+	];
+	listaMusica[5]  = [
+			{
+				source : 'public/music/lunes/Hotel Carlifornia.mp3',
+				name: 'Hotel Carliforni',
+			},
+			{
+				source : 'public/music/lunes/Killing me softly.mp3',
+				name: 'Killing me softly',
+			}
+	];
+	listaMusica[6]  = [
+			{
+				source : 'public/music/lunes/Hotel Carlifornia.mp3',
+				name: 'Hotel Carliforni',
+			},
+			{
+				source : 'public/music/lunes/Killing me softly.mp3',
+				name: 'Killing me softly',
+			}
+	];
+
+	let playList = listaMusica[fecha.getDay() - 1]
+
+	for (var i = playList.length - 1; i >= 0; i--) {
+		$('#bbaudio').prepend('<source src="'+ playList[i].source +'" type="audio/mp3"/>');
+		$('#bb-listplayer').prepend('<li class="bb-music" onclick="loadTrack('+ i +')">'+playList[i].name+'</li>');
+	}
+	
+
+
 }
